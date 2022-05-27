0fc2dfdce0c842847fe32dec06cae049969c9a92
EC2 Default User <ec2-user@ip-172-31-21-48.us-west-2.compute.internal>
2022-05-27 03:44:08 +0000

    Last changes

--- application/data/handlePostMoveNotification.js
+++ application/data/handlePostMoveNotification.js
@@ -6,11 +6,11 @@ const sendMessage = require('./sendMessage')
 const handlePostMoveNotification = async ({ game, mover, opponent }) => {
   // Handle when game is finished
   if ((game.A1 == game.A2  && game.A2 == game.A3 && game.A2 != ' ') || (game.B1 == game.B2 && game.B2 == game.B3 && game.B2 != ' ') || (game.C1 == game.C2 && game.C2 == game.C3 && game.C2 != ' ') || (game.A1 == game.B1 && game.B1 && game.C1 && game.B1 != ' ') || (game.A2 == game.B2 && game.B2 == game.C2 && game.B2 != ' ') || (game.A3 == game.B3 && game.B3 == game.C3 && game.B3 != ' ') || (game.A1 == game.B2 && game.B2 == game.C3 && game.B2 != ' ') || (game.A3 == game.B2 && game.B2 == game.C1 && game.B2 != ' ')) {
-    const winnerMessage = `You beat ${mover.username} in a game of Tic-Tac-Toe!`
-    const loserMessage = `Ahh, you lost to ${opponent.username} in Tic-Tac-Toe.`
+    const winnerMessage = `You beat ${opponent.username} in a game of Tic-Tac-Toe!`
+    const loserMessage = `Ahh, you lost to ${mover.username} in Tic-Tac-Toe.`
     await Promise.all([
-      sendMessage({ email: `${opponent.email}`, message: winnerMessage }),
-      sendMessage({ email: `${mover.email}`, message: loserMessage })
+      sendMessage({ email: `${mover.email}`, message: winnerMessage }),
+      sendMessage({ email: `${opponent.email}`, message: loserMessage })
     ])
 
     return
